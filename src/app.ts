a7be7f7c866af03c34c5b1cfbc0a9cd15fdb574c
Gabriel Biasoli <bielbiasoli33@icloud.com>
2022-02-23 17:34:02 -0300

    Adds '/products' POST and GET routes

--- src/app.ts
+++ src/app.ts
@@ -4,6 +4,8 @@ import joiError from './controllers/middlewares/joiError';
 import domainError from './controllers/middlewares/domainError';
 import serverError from './controllers/middlewares/serverError';
 import generateToken from './controllers/middlewares/generateToken';
+import * as ProductController from './controllers/productController';
+import authenticateToken from './controllers/middlewares/authenticateToken';
 
 const app = express();
 
@@ -11,6 +13,8 @@ app.use(express.json());
 
 app.post('/users', UserController.create, generateToken);
 app.post('/login', UserController.login, generateToken);
+app.post('/products', authenticateToken, ProductController.create);
+app.get('/products', authenticateToken, ProductController.getAll);
 
 app.use(joiError);
 app.use(domainError);
